be1cc19e80c116f9dbfb9b7d7b8503f5686ed31b
Aditya Khatry <amkhatry@gmail.com>
2023-05-10 18:00:17 +1000

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        public string Icon { get; set; } = string.Empty;
+
+        public string Type { get; set; } = "Expense";
+    }
+}
